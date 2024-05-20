05452fc42ee118c7226b660c79dba5860d10250a
Vu Huy <Vu Huy@DESKTOP-29PPQCA>
2024-05-20 22:32:58 +0700

    Add project files.

---
+++ B2/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2
+{
+    public abstract class Person
+    {
+        private string name;
+        private string address;
+
+        public Person(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public abstract void display();
+    }
+
+    public class Employee : Person
+    {
+        private int salary;
+        public Employee(string name, string address, int salary) : base(name, address)
+        {
+            this.salary = salary;
+        }
+        public int Salary { get { return salary; } set { salary = value; } }
+
+        public override void display()
+        {
+            Console.WriteLine($"Name: {Name}, Address: {Address}, Salary: {salary}");
+        }
+    }
+
+    public class Customer : Person
+    {
+        private int balance;
+        public Customer(string name, string address, int balance) : base(name, address)
+        {
+            this.balance = balance;
+        }
+        public int Balance { get { return balance; } set { balance = value; } }
+
+        public override void display()
+        {
+            Console.WriteLine($"Name: {Name}, Address: {Address}, Balance: {balance}");
+        }
+    }
+}
---
+++ B2/Program.cs
@@ -0,0 +1,130 @@
+﻿// See https://aka.ms/new-console-template for more information
+using B2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Employee> employees = new List<Employee>();
+        List<Customer> customers = new List<Customer>();
+        bool exit = false;
+        while (!exit)
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Add Employee");
+            Console.WriteLine("2. Add Customer");
+            Console.WriteLine("3. Find employee with highest salary");
+            Console.WriteLine("4. Find customer with lowest balance ");
+            Console.WriteLine("5. Find employee by name");
+            Console.WriteLine("6. Exit");
+            int option;
+            while (!int.TryParse(Console.ReadLine(), out option))
+            {
+                Console.WriteLine("Invalid output.Please enter a number.");
+            }
+            switch (option)
+            {
+                case 1:
+                    AddEmployee(employees);
+                    break;
+                case 2:
+                    AddCustomer(customers);
+                    break;
+                case 3:
+                    FindEmployeeHighestSalary(employees);
+                    break;
+                case 4:
+                    FindCustomerLowestBalance(customers);
+                    break;
+                case 5:
+                    FindEmployeeByName(employees);
+                    break;
+                case 6:
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Please choose again.");
+                    break;
+            }
+        }
+    }
+
+    public static void AddEmployee(List<Employee> employees)
+    {
+        try
+        {
+            Console.WriteLine("Enter name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter address: ");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter salary: ");
+            int salary = int.Parse(Console.ReadLine());
+            employees.Add(new Employee(name, address, salary));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter correct data.");
+        }
+    }
+
+    public static void AddCustomer(List<Customer> customers)
+    {
+        try
+        {
+            Console.WriteLine("Enter name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter address: ");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter balance: ");
+            int balance = int.Parse(Console.ReadLine());
+            customers.Add(new Customer(name, address, balance));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter correct data.");
+        }
+    }
+
+    public static void FindEmployeeHighestSalary(List<Employee> employees)
+    {
+        if (employees.Count == 0)
+        {
+            Console.WriteLine("No employee found.");
+            return;
+        }
+
+        var highestSalaryEmployees = employees.OrderByDescending(e => e.Salary).FirstOrDefault();
+        Console.WriteLine("Employee with the highest salary: ");
+        highestSalaryEmployees.display();
+    }
+
+    public static void FindCustomerLowestBalance(List<Customer> customers)
+    {
+        if (customers.Count == 0)
+        {
+            Console.WriteLine("No customer found.");
+            return;
+        }
+
+        var lowestBalanceCustomer = customers.OrderBy(c => c.Balance).FirstOrDefault();
+        Console.WriteLine("Customer with the lowest balace: ");
+        lowestBalanceCustomer.display();
+    }
+
+    public static void FindEmployeeByName(List<Employee> employees)
+    {
+        Console.Write("Enter name: ");
+        string s = Console.ReadLine();
+        
+        var foundEmployees = employees.Where(e => e.Name.Contains(s, StringComparison.OrdinalIgnoreCase)).ToList();
+        if (foundEmployees.Count == 0)
+        {
+            Console.WriteLine("No employees found with the given name.");
+        } else
+        {
+            foreach (var employee in foundEmployees)
+            {
+                employee.display();
+            }
+        }
+    }
+}
